b88502b66e4705032ac17694c6249899a9ad3dbf
liudeng <liudengchn@gmail.com>
2020-12-11 00:31:24 +0800

    [add] leetcode-714-买卖股票的最佳时机-含手续费

---
+++ src/main/java/q714/Solution.java
@@ -0,0 +1,25 @@
+package q714;
+
+/**
+ * 买卖股票的最佳时机，含手续费
+ *
+ * @author liudeng
+ * @date 2020/12/11 上午12:26
+ * @since
+ **/
+public class Solution {
+    public int maxProfit(int[] prices, int fee) {
+        // 第一天不持有股票的利润
+        int cash = 0;
+        // 第一天持有股票的利润
+        int hold = -prices[0];
+        int length = prices.length;
+
+        for (int i = 1; i < length; i++) {
+            cash = Math.max(cash, hold + prices[i] - fee);
+            hold = Math.max(hold, cash - prices[i]);
+        }
+        return cash;
+
+    }
+}
